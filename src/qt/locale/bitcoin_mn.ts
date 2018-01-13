e064f2b39e7ab90bdca2a16e0b5d191109cec285
Anthony Cloudy <acloudy@mail.smu.edu>
2018-01-13 13:42:23 -0800

    Changed the name of the coin.

--- src/qt/locale/bitcoin_mn.ts
+++ src/qt/locale/bitcoin_mn.ts
@@ -126,8 +126,8 @@
         <translation>Оношилгоо ба засварын консолыг онгойлго</translation>
     </message>
     <message>
-        <source>Litecoin</source>
-        <translation>Litecoin</translation>
+        <source>Daichmark</source>
+        <translation>Daichmark</translation>
     </message>
     <message>
         <source>Wallet</source>
